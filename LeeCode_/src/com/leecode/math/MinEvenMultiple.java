beeb0ec732b171752b51ea1298dbd3bba76755da
zly321 <2690993664@qq.com>
2025-06-29 11:49:40 +0800

    2413:最小偶倍数

---
+++ LeeCode_/src/com/leecode/math/MinEvenMultiple.java
@@ -0,0 +1,24 @@
+package com.leecode.math;
+
+import java.util.Scanner;
+
+/**
+ * @author 张立业
+ * @version 1.0
+ */
+public class MinEvenMultiple {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number = ");
+        int n = sc.nextInt();
+        System.out.println(new Solution().smallestEvenMultiple(n));
+
+    }
+
+    static class Solution {
+        public int smallestEvenMultiple(int n) {
+            //如果 n 是偶数，则最小公倍数就是n, 如果是奇数，则是2*n
+            return (n % 2 == 0 ? n : n * 2);
+        }
+    }
+}
