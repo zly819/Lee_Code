64aafaa6e39b4960055c89f0f7b9226ae76aaf21
zly321 <2690993664@qq.com>
2025-06-29 11:39:35 +0800

    2235:两整数相加

---
+++ LeeCode_/src/com/leecode/math/add.java
@@ -0,0 +1,26 @@
+package com.leecode.math;
+
+import java.util.Scanner;
+
+/**
+ * @author 张立业
+ * @version 1.0
+ */
+public class add {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("num1= ");
+        int value01 = scanner.nextInt();
+        System.out.print("num2= ");
+        int value02 = scanner.nextInt();
+        Solution solution = new Solution();
+        System.out.println(solution.sum(value01, value02));
+
+    }
+
+    static class Solution {
+        public int sum(int num1, int num2) {
+            return num1 + num2;
+        }
+    }
+}
